7984869b6c69dc8a1486781f65c527a56f5d804b
AjiSetya <setyaaji07@gmail.com>
2019-03-16 04:36:17 +0700

    membuat fungsi MainActivity

--- app/src/main/java/com/frogobox/finalprojecteudeka/views/activities/MainActivity.java
+++ app/src/main/java/com/frogobox/finalprojecteudeka/views/activities/MainActivity.java
@@ -1,9 +1,14 @@
 package com.frogobox.finalprojecteudeka.views.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.frogobox.finalprojecteudeka.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +16,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    //openFragment(new BreedFragment);
+                    return true;
+                case R.id.navigation_add:
+                    //openFragment(new FavoriteFragment);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    private void openFragment(Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.container, fragment)
+                .commit();
     }
 }
