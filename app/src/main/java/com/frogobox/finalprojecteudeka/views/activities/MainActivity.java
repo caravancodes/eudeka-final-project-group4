ed69ce288ff5b788d5bb2ac9857d2d3be23bb4d6
amirisback <faisalamircs@gmail.com>
2019-03-19 15:38:30 +0700

    1.3 Merubah BreedFragment Menjadi Cat Fragment

--- app/src/main/java/com/frogobox/finalprojecteudeka/views/activities/MainActivity.java
+++ app/src/main/java/com/frogobox/finalprojecteudeka/views/activities/MainActivity.java
@@ -28,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    //openFragment(new BreedFragment);
+                    //openFragment(new CatFragment);
                     return true;
                 case R.id.navigation_add:
                     //openFragment(new FavoriteFragment);
--- app/src/main/java/com/frogobox/finalprojecteudeka/views/fragments/CatFragment.java
+++ app/src/main/java/com/frogobox/finalprojecteudeka/views/fragments/CatFragment.java
@@ -14,10 +14,10 @@ import com.frogobox.finalprojecteudeka.R;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class BreedFragment extends Fragment {
+public class CatFragment extends Fragment {
 
 
-    public BreedFragment() {
+    public CatFragment() {
         // Required empty public constructor
     }
 
@@ -26,7 +26,7 @@ public class BreedFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_breed, container, false);
+        return inflater.inflate(R.layout.fragment_cat, container, false);
     }
 
 }
