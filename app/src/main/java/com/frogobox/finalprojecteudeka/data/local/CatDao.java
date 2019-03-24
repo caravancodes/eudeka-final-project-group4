52cf490fd74f6512328b742df1e3e5d24f22b222
jessica <jnathania@indodana.com>
2019-03-25 01:22:16 +0700

    Remove unused/redundant libraries, add cat local data

--- app/src/main/java/com/frogobox/finalprojecteudeka/data/local/CatDao.java
+++ app/src/main/java/com/frogobox/finalprojecteudeka/data/local/CatDao.java
@@ -1,5 +1,14 @@
 package com.frogobox.finalprojecteudeka.data.local;
 
+import com.frogobox.finalprojecteudeka.models.Cat;
+import com.frogobox.finalprojecteudeka.models.CatDetail;
+
+import java.util.List;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
 /**
  * Created by Faisal Amir
  * FrogoBox Inc License
@@ -17,5 +26,11 @@ package com.frogobox.finalprojecteudeka.data.local;
  * -----------------------------------------
  * id.amirisback.frogobox
  */
+@Dao
 public interface CatDao {
+    @Query("SELECT * FROM cat")
+    List<Cat> getCats();
+
+    @Insert
+    void insertCatData(List<Cat> data);
 }
