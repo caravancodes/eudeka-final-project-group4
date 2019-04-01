faa91e872ae6036a81200f0c4865b30383baa9d7
amirisback <faisalamircs@gmail.com>
2019-04-01 23:26:08 +0700

    API KEY Cat

--- app/src/main/java/com/frogobox/finalprojecteudeka/data/remote/ApiUrl.java
+++ app/src/main/java/com/frogobox/finalprojecteudeka/data/remote/ApiUrl.java
@@ -24,7 +24,10 @@ public class ApiUrl {
 
     static final class ListApiUrl {
 
+
+
         static final String BASE_URL = "https://api.thecatapi.com/";
+        static final String API_KEY = "6fb1a8d8-dd31-4592-8ef6-8e35b8a7efdd";
         static final String PREFIX = "v1/";
 
 //        public static final String URL_LONG = "https://api.thecatapi.com/v1/breeds";
